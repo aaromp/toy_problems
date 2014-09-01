dc2150375470bccbf5e88a42158075ec8d520b41
aaromp <aaromp@gmail.com>
2014-08-31 23:06:00 -0700

    add kata_example_twist.js

---
+++ codewars/javascript/8kyu/kata_example_twist.js
@@ -0,0 +1,9 @@
+// This is an easy twist to the kata example.
+
+// Add the value "codewars" to the websites array 1,000 times.
+
+var websites;
+
+websites = [];
+
+while (websites.length < 1000) websites.push('codewars');

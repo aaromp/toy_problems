115be36f33257000390ad7cc9cfd3e7c3029b90d
aaromp <aaromp@gmail.com>
2014-08-31 21:40:21 -0700

    add swap_values.js first attempt

---
+++ codewars/javascript/8kyu/swap_values.js
@@ -0,0 +1,13 @@
+// I would like to be able to pass an array with two elements to my swapValues 
+// function to swap the values. However it appears that the values 
+// aren't changing.
+
+// Can you figure out what's wrong here?
+
+function swapValues(array) {
+  var temporary;
+
+  temporary = array[0];
+  array[0] = array[1];
+  array[1] = temporary;
+}

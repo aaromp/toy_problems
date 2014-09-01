59a400871fc31ddf0496c900866c6a744b2302fe
aaromp <aaromp@gmail.com>
2014-09-01 11:50:49 -0700

    add name_array_capping.js

---
+++ codewars/javascript/7kyu/name_array_capping.js
@@ -0,0 +1,13 @@
+// Create a method that accepts an array of names, and returns an array of each 
+// name with its first letter capitalized.
+
+// example
+
+// capMe(['jo', 'nelson', 'jurie'])     // returns ['Jo', 'Nelson', 'Jurie']
+// capMe(['KARLY', 'DANIEL', 'KELSEY']) // returns ['Karly', 'Daniel', 'Kelsey']
+
+function capMe(names) {
+  return names.map(function(element) {
+    return element[0].toUpperCase() + element.slice(1).toLowerCase();
+  });
+}

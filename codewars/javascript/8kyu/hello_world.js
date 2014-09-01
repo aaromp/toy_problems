d7e9273086da85e7c289d4981e5306729509495e
aaromp <aaromp@gmail.com>
2014-08-31 23:17:37 -0700

    add hello_world.js

---
+++ codewars/javascript/8kyu/hello_world.js
@@ -0,0 +1,10 @@
+// Make a simple function called greet that returns the most-famous 
+// "hello world!".
+
+// Although this has been thought as a simple exercise for beginners, other 
+// people chimed in and loved to show off how wonderfully complicated you can 
+// write "hello world!" - so go for it, create the most beautiful "hello world!"
+
+function greet() {
+  return 'hello world!';
+}

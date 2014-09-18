3060f02c68dc450fc06a32e6131d654d17ab2323
aaromp <aaromp@gmail.com>
2014-09-18 13:44:09 -0700

    fix off by one error

--- project_euler/swift/euler_001.swift
+++ project_euler/swift/euler_001.swift
@@ -1,11 +1,13 @@
 let title = "Multiples of 3 and 5"
 
-//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+// get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-//Find the sum of all the multiples of 3 or 5 below 1000.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+let max = 1000
 var result = 0
 
-for index in 1...1000 {
+for index in 1...max-1 {
     if index % 3 == 0 || index % 5 == 0 {
         result += index
     }

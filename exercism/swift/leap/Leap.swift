891e1ca324af47639a267e49fa8f6bdaba1f0846
Aaron Ward <aaromp@gmail.com>
2014-09-24 17:11:46 -0700

    add leap second iteration

--- exercism/swift/leap/Leap.swift
+++ exercism/swift/leap/Leap.swift
@@ -8,28 +8,21 @@
 
 import Foundation
 
-class Year {
-    var year = 0
-    var isLeapYear = false
-    
-    init(calendarYear: Int) {
-        self.year = calendarYear
-        self.isLeapYear = self.determineIfLeapYear(calendarYear)
-    }
-    
-    func determineIfLeapYear(calendarYear: Int) -> Bool {
+struct Year {
+    let calendarYear: Int
+    var isLeapYear : Bool {
         if calendarYear % 400 == 0 {
             return true
         }
-        
+            
         if calendarYear % 100 == 0 {
             return false
         }
-        
+            
         if calendarYear % 4 == 0  {
             return true
         }
-        
+            
         return false
     }
 }
